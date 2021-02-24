b5b40bcaa58a7775811c8e08514f443a365c59d9
bruceyboy86 <bruced247@gmail.com>
2021-02-24 22:13:17 +0000

    basic type annotations and inference

--- 02/demos/demos/Before/multimath/app/app.ts
+++ 02/demos/demos/Before/multimath/app/app.ts
@@ -1,9 +1,15 @@
 
 function startGame() {
 
+    let playerName: string = 'Bruce'
+    logPlayer(playerName)
     // comment
     let messagesEl = document.getElementById('messages')
     messagesEl!.innerText = 'wassaaap';
+}
+
+function logPlayer(name: string) {
+    console.log(`New game starting for player: ${name}`)
 }
 
 document.querySelector('#startGame')?.addEventListener('click', startGame); 
