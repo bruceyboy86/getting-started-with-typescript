e4bc5c3230726414016be29b56565c65ec6e750d
bruceyboy86 <bruced247@gmail.com>
2021-03-01 22:32:52 +0000

    adding type annotations

--- 02/demos/demos/Before/multimath/app/app.ts
+++ 02/demos/demos/Before/multimath/app/app.ts
@@ -1,15 +1,29 @@
 
 function startGame() {
 
-    let playerName: string = 'bruce'
+    let playerName: string | undefined = getInputValue('playername')
     logPlayer(playerName)
-    // comment
-    let messagesEl = document.getElementById('messages')
-    messagesEl!.innerText = 'wassaaap' ;
+    postScore(80, playerName)
 }
 
-function logPlayer(name:string ): void {
+function logPlayer(name:string = 'Multimath player'): void {
     console.log(`New game starting for player: ${name}`)
+}
+
+function getInputValue(elementId:string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId)
+
+    if (inputElement.value === '') {
+        return undefined
+    } else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score:number, playerName: string = 'Multimath player'):void {
+    const scoreElement:HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+    
 }
 
 document.querySelector('#startGame')!.addEventListener('click', startGame); 
