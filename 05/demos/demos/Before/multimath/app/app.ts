2f9b9c89021d067ed44864263410d0aa1adc989d
bruceyoy86 <bruced247@gmail.com>
2021-04-10 21:05:18 +0100

    half way through - 5: refactoring the  demo app with classes

--- 05/demos/demos/Before/multimath/app/app.ts
+++ 05/demos/demos/Before/multimath/app/app.ts
@@ -14,18 +14,6 @@ function logPlayer(name: string = 'MultiMath Player'): void {
   console.log(`New game starting for player: ${name}`);
 }
 
-function getInputValue(elementID: string): string | undefined {
-
-  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
-
-  if (inputElement.value === '') {
-    return undefined;
-  }
-  else {
-    return inputElement.value;
-  }  
-}
-
 function postScore(score: number, playerName: string = 'MultiMath Player'): void {
 
   let logger: (value: string) => void;
