b4b4c189f9f91e026fe7e98808e722e6a5c5d5a5
bruceyoy86 <bruced247@gmail.com>
2021-04-13 22:27:20 +0100

    added displayGame()

--- 05/demos/demos/Before/multimath/app/game.ts
+++ 05/demos/demos/Before/multimath/app/game.ts
@@ -8,4 +8,24 @@ class Game {
 
     constructor(public player: Player, public problemCount: number, public factor: number) {
     }
+
+    displayGame(): void{
+
+        // create HTML for the current game
+        let gameForm: string = '';
+        for (let i = 1; i < this.problemCount; i++) {
+            gameForm += '<div class="form-group">';
+            gameForm += '<label for="answer'+i+'" class="col-sm-2 control-label">';
+            gameForm += String(this.factor)+' x '+i+' = </label>';
+            gameForm += '<div class="col-sm-1"><input type="text" class="form-control" id="answer'+i+'" size="5"/></div>';
+            gameForm += '</div>';            
+        }
+
+        // add the new game to the page
+        const getElement: HTMLElement = document.getElementById('game')!;
+        getElement.innerHTML = gameForm;
+
+        // enable the calculate score button
+        document.getElementById('calculate')!.removeAttribute('disabled');
+    }
 }
